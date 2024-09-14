838e9579d66d7e6d3db7f532912adbcef81e0fd8
jiahai <Jhail@foxmail.com>
2024-09-14 19:18:10 +0800

    feat: 添加打包类型的脚本

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,11 +3,13 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import path from 'path';
-import { packageRoot } from './utils/paths';
+import { distRoot, packageRoot, projectRoot } from './utils/paths';
 import { sync } from 'fast-glob';
 import { buildConfig } from './utils/config';
-import { pathRewriter, withTaskName } from './utils';
+import { withTaskName } from './utils';
 import { series } from 'gulp';
+import { Project, SourceFile } from 'ts-morph';
+import fs from 'fs/promises';
 
 const build = async () => {
   const packages = sync('*', {
@@ -36,5 +38,67 @@ const build = async () => {
   return Promise.all(tasks);
 };
 
-// export const buildPackages = build();
-export const buildPackages = series(withTaskName('eachPackage:build', build));
+/* 生成ts的声明文件 */
+async function genTypes() {
+  const packages = sync('*', {
+    cwd: packageRoot,
+    onlyDirectories: true
+  });
+  const tasks = packages.map(async (pkg) => {
+    const workDir = path.resolve(packageRoot, pkg);
+    const outDir = path.resolve(distRoot, pkg, 'dist/types');
+    const project = new Project({
+      tsConfigFilePath: path.resolve(projectRoot, 'tsconfig.json'),
+      skipAddingFilesFromTsConfig: true,
+      compilerOptions: {
+        declaration: true,
+        allowJs: true,
+        emitDeclarationOnly: true,
+        noEmitOnError: true,
+        outDir,
+        baseUrl: projectRoot,
+        skipLibCheck: true,
+        strict: false
+      }
+    });
+    const projectFilePaths = sync('**/*', {
+      cwd: workDir,
+      onlyFiles: true,
+      absolute: true
+    });
+    const sourceFiles: SourceFile[] = [];
+    await Promise.all(
+      projectFilePaths.map(async (file) => {
+        if (file.endsWith('.ts')) {
+          const sourceFile = project.addSourceFileAtPath(file);
+          sourceFiles.push(sourceFile);
+        }
+      })
+    );
+    const diagnostics = project.getPreEmitDiagnostics();
+    console.log(project.formatDiagnosticsWithColorAndContext(diagnostics));
+    await project.emit({
+      emitOnlyDtsFiles: true
+    });
+
+    const sourceFileTasks = sourceFiles.map(async (sourceFile: SourceFile) => {
+      const emitOutput = sourceFile.getEmitOutput();
+      const outputFileTasks = emitOutput.getOutputFiles().map(async (outputFile) => {
+        const filepath = outputFile.getFilePath();
+        await fs.mkdir(path.dirname(filepath), {
+          recursive: true
+        });
+        const outputFileText = outputFile.getText();
+        await fs.writeFile(filepath, outputFileText);
+      });
+      await Promise.all(outputFileTasks);
+    });
+    await Promise.all(sourceFileTasks);
+  });
+  return Promise.all(tasks);
+}
+
+export const buildPackages = series(
+  withTaskName('eachPackage:build', build),
+  withTaskName('eachPackage:genTypes', genTypes)
+);
